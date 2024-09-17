b01fb8d50284005caff4a449f26ea9dfad2a0c80
Niharikas257 <158223705+Niharikas257@users.noreply.github.com>
2024-09-17 12:53:41 -0400

    Time: 13 ms (54.81%) | Memory: 11.7 MB (34.03%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -10,7 +10,7 @@ class Solution(object):
             return 2
         # Allocate memory to the array
         ways = [0] * (n+1)  
-        ways[0] = 0
+        
         ways[1] = 1
         ways[2] = 2
         for i in range(3, n+1):
